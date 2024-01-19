2197edffb5d074cb088f3f292139041f7b582dcc
Amruta <amruta@Amrutas-MacBook-Pro.local>
2024-01-19 16:56:19 +0530

    Testing PR Agent

--- app1/models.py
+++ app1/models.py
@@ -1,3 +1,10 @@
+# myapp/models.py
 from django.db import models
 
-# Create your models here.
+class YourModel(models.Model):
+    # Your model fields go here
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
--- app1/views.py
+++ app1/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+# myapp/views.py
+from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
 def home(request):
     return render(request,'home.html')
+
+
+def index(request):
+    return HttpResponse("Hello, this is your app's index view.")
